2ebc64aae058e859c53e35ef97c63e23c9a25b0b
filippos <filippos29@hotmail.com>
2026-02-23 18:25:43 +0100

    feat(login, registration): added login and registration forms

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransactionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TransactionRepository::class)]
+#[ORM\Table(name: '`transaction`')]
+class Transaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $PaymentMethod = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPaymentMethod(): ?string
+    {
+        return $this->PaymentMethod;
+    }
+
+    public function setPaymentMethod(string $PaymentMethod): static
+    {
+        $this->PaymentMethod = $PaymentMethod;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(string $Category): static
+    {
+        $this->Category = $Category;
+
+        return $this;
+    }
+}
