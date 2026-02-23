4d5f584ab5e8bfb0dac8548924e742a915842ffc
filippos <filippos29@hotmail.com>
2026-02-23 19:20:33 +0100

    crud controllers and payment methods

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -14,8 +14,8 @@ class Transaction
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column]
-    private ?int $UserId = null;
+    //#[ORM\Column]
+    //private ?int $UserId = null;
 
     #[ORM\Column(length: 255)]
     private ?string $PaymentMethod = null;
